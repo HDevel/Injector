f990fd670162b213f64b955ba088d9f2a45d711a
HDevel <H.Devel@gmail.com>
2016-01-22 15:22:06 +0300

    add files

---
+++ app.js
@@ -0,0 +1,12 @@
+﻿var http = require('http'),
+    static = require('node-static');
+
+var server = http.createServer(function(req, res) {
+    req.addListener('end', function () {
+        new static.Server('./public').serve(req, res);
+    }).resume();
+});
+
+server.listen(5000, function() {
+    console.log('Listening at: http://localhost:5000');
+});
---
+++ public/common/injector.js
@@ -0,0 +1,112 @@
+window.onload = function(){
+    injector.init.apply(injector)
+};
+var injector = {
+    params: {
+        baseUrl: 'blocks/',
+        techs: ['html', 'css', 'js'],
+        style_id: 'inject_style',
+        style_done: {},
+        style: document.createElement('style')
+    },
+    init: function () {
+        this.params.style.setAttribute('id', this.params.style_id);
+
+        this.buildInjector();
+    },
+    buildInjector: function () {
+        var self = this;
+        var injectors = this.seatchInjectors();
+        if(injectors.length == 0){
+            document.getElementsByTagName('head')[0].appendChild(this.params.style);
+        }
+        injectors.forEach(function (node) {
+            node.techs.forEach(function (tech) {
+                switch (tech.split('.').pop()){
+                    case 'html':
+                        self.loadFile(node.url, tech, function (string) {
+                            var content = string.match(/{{[^}]+}}/g);
+                            if(content){
+                                content.forEach(function(v){
+                                    var replace = node.node.getAttribute(v.slice(2).slice(0,-2));
+                                    string = string.replace(v, replace ? replace : '');
+                                });
+                            }
+                            var div = document.createElement('div');
+                            div.innerHTML = string;
+                            var newNode = div.firstChild;
+                            var currentClass = newNode.getAttribute('class').split(' ');
+                            if(node.mods){
+                                node.mods.forEach(function (m) {
+                                    newNode.className += ' ' + currentClass + '_' + m;
+                                });
+                            }
+
+                            self.replaceInjectorWith(node.node, newNode);
+                            self.buildInjector();
+                        });
+                        break
+                    case 'css':
+                        var id = node.name + tech;
+                        if(!self.params.style_done[id]){
+                            self.params.style_done[id] = true;
+                            self.params.style.insertAdjacentHTML('beforeend', '@import url(' + node.url + tech + '?' + Math.random() + ');');
+                        }
+                        break
+                }
+            })
+        });
+    },
+    seatchInjectors: function () {
+        var self = this;
+        var nodes = [];
+        var injList = injector.getInjectorList();
+        for (var i = 0; i < injList.length; i++) {
+            if(!injList[i].getAttribute('injecting')){
+                injList[i].setAttribute('injecting', '1');
+                var name = injList[i].getAttribute('inject');
+                var url = this.params.baseUrl + name + '/' + name;
+                nodes.push(
+                    {
+                        node: injList[i],
+                        techs: [],
+                        name: name,
+                        url: url
+                    }
+                );
+            }
+        }
+
+        nodes.forEach(function (n) {
+            self.params.techs.forEach(function (t) {
+                if (n.node.getAttribute(t) != null) {
+                    n.techs.push('.' + t);
+                    var mods = n.node.getAttribute('mod');
+                    if (mods != null && t != 'html') {
+                        n.mods = mods.split(' ');
+                        n.mods.forEach(function (m) {
+                            n.techs.push('_' + m + '.' + t);
+                        });
+
+                    }
+                }
+            });
+        });
+        return nodes;
+    },
+    getInjectorList: function () {
+        return document.getElementsByTagName('inject');
+    },
+    replaceInjectorWith: function (oldNode, newNode) {
+        oldNode.parentNode.replaceChild(newNode, oldNode);
+    },
+    loadFile: function (url, tech, callback, error) {
+        var url = url + tech + '?' + Math.random();
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function (rsp) {
+            callback(rsp.target.responseText);
+        };
+        xhr.send();
+    }
+};
